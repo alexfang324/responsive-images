f0752d6f1db742d233ffff6e6c68ee7fb5936dc2
alexfang324 <106351391+alexfang324@users.noreply.github.com>
2024-01-11 19:02:58 -0800

    implemented hamburger menu animation

--- scripts/menu.js
+++ scripts/menu.js
@@ -4,7 +4,8 @@
 const body = document.body;
 const btnMenu = document.getElementById('btn-menu');
 const bar = document.getElementById('bar');
-const nav = document.getElementById('main-navigation');
+const nav = document.querySelector('.menu-wrapper');
+const menu = document.querySelector('.menu');
 
 btnMenu.addEventListener('click', openMenu);
 // Prevents the focus state from activating
@@ -14,5 +15,8 @@ btnMenu.addEventListener('mousedown', function (e) {
 
 function openMenu() {
   btnMenu.classList.toggle('open');
+  btnMenu.classList.toggle('hamburger-guide');
   nav.classList.toggle('open');
+  nav.classList.toggle('active');
+  menu.classList.toggle('menu-active');
 }
